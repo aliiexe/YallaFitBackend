a2f43c0b816a12db7a6705beb8d31f1384b91ac6
aliiexe <alibusinessbourak@gmail.com>
2025-12-08 00:02:14 +0100

    Initial commit

---
+++ YallaFit/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace YallaFit.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
